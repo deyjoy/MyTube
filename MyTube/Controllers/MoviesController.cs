a73978eddb0f50dd315aad12365cc7db69d7914c
deyjoy <joydey@gmail.com>
2019-08-30 21:16:36 -0400

    included new stuffs in view and modified layout

--- MyTube/Controllers/MoviesController.cs
+++ MyTube/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using MyTube.Models;
+using MyTube.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,36 @@ namespace MyTube.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie()
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
             {
-                Name = "Shrek!"
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
-
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
     }
 }
